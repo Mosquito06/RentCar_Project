c51d073258d08d5bca0b264dd2cd4b102b01abc7
DaHee Choi <ekgml9904@naver.com>
2017-11-13 16:13:59 +0900

    관리자 차량 ui 수정

--- src/main/java/kr/or/dgit/RentCar_Project/content/CarModelContent.java
+++ src/main/java/kr/or/dgit/RentCar_Project/content/CarModelContent.java
@@ -3,61 +3,55 @@ package kr.or.dgit.RentCar_Project.content;
 import javax.swing.JPanel;
 import kr.or.dgit.RentCar_Project.component.TextFieldComponent;
 import javax.swing.JTextField;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JLabel;
 
 @SuppressWarnings("serial")
 public class CarModelContent extends JPanel {
-	private JTextField textField;
-
+	private TextFieldComponent cmCode;
+	private TextFieldComponent carModel;
 	public CarModelContent() {
 		setLayout(null);
 		
-		TextFieldComponent carModelCodePanel = new TextFieldComponent("차종 코드");
-		carModelCodePanel.setBounds(5, 31, 230, 30);
-		add(carModelCodePanel);
+		cmCode = new TextFieldComponent("차종 코드");
+		cmCode.setBounds(5, 35, 230, 30);
+		add(cmCode);
 		
-		TextFieldComponent carModelPanel = new TextFieldComponent("차종");
-		carModelPanel.setBounds(5, 73, 230, 30);
-		add(carModelPanel);
-		
-		JPanel imgPanel = new JPanel();
-		imgPanel.setLayout(null);
-		imgPanel.setBounds(244, 21, 224, 82);
-		add(imgPanel);
-		
-		textField = new JTextField();
-		textField.setColumns(10);
-		textField.setBounds(55, 10, 169, 29);
-		imgPanel.add(textField);
-		
-		JButton btnImgDelete = new JButton("이미지 삭제");
-		btnImgDelete.setBounds(112, 49, 112, 29);
-		imgPanel.add(btnImgDelete);
-		
-		JButton btnImgAdd = new JButton("이미지 추가");
-		btnImgAdd.setBounds(0, 49, 112, 29);
-		imgPanel.add(btnImgAdd);
-		
-		JLabel label = new JLabel("경로");
-		label.setBounds(0, 10, 47, 29);
-		imgPanel.add(label);
+		carModel = new TextFieldComponent("차종");
+		carModel.setBounds(5, 80, 230, 30);
+		add(carModel);
 		
 		JButton btnAdd = new JButton("추가");
-		btnAdd.setBounds(498, 15, 66, 23);
+		btnAdd.setBounds(250, 20, 66, 23);
 		add(btnAdd);
 		
 		JButton btnUpdate = new JButton("수정");
-		btnUpdate.setBounds(498, 40, 66, 23);
+		btnUpdate.setBounds(250, 45, 66, 23);
 		add(btnUpdate);
 		
 		JButton btnDelete = new JButton("삭제");
-		btnDelete.setBounds(498, 65, 66, 23);
+		btnDelete.setBounds(250, 70, 66, 23);
 		add(btnDelete);
 		
 		JButton btnCancel = new JButton("취소");
-		btnCancel.setBounds(498, 105, 66, 23);
+		btnCancel.setBounds(250, 110, 66, 23);
+		btnCancel.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				setCarModelValueClear();
+				
+			}
+		});
 		add(btnCancel);
 
+	}
+	public void setCarModelValueClear(){
+		cmCode.setTextValue("");
+		carModel.setTextValue("");
 	}
 }
